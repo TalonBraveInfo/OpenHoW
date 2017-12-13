f8b90f8ffc4ceafc8f6cde61790f583d00681c9c
hogsy <markelswo@gmail.com>
2017-12-13 21:20:40 +0000

    Added map names + descriptions

--- libpork/map.c
+++ libpork/map.c
@@ -16,6 +16,43 @@
  */
 #include "engine.h"
 
+typedef struct MapEntry {
+    const char *name;
+    const char *description;
+} MapEntry;
+
+// for now these are hard-coded, but
+// eventually we'll do this through a
+// script instead
+MapEntry maps_list[]={
+        {"camp", "Boot camp"},
+        {"estu", "The War Foundation"},
+        {"road", "Routine Patrol"},
+        {"trench", "Trench Warfare"},
+        {"devi", "Morning Glory!"},
+        {"rumble", "Island Invasion"},
+        {"zulus", "Under Siege"},
+        {"twin", "Communication Breakdown"},
+        {"sniper", "The Spying Game"},
+        {"mashed", "The Village People"},
+        {"guns", "Bangers 'N' Mash"},
+        {"liberate", "Saving Private Rind"},
+        {"oasis", "Just Deserts"},
+        {"fjords", "Glacier Guns"},
+        {"eyrie", "Battle Stations"},
+        {"bay", "Fortified Swine"},
+        {"medix", "Over The Top"},
+        {"bridge", "Geneva Convention"},
+        {"desval", "I Spy..."},
+        {"snake", "Chemical Compound"},
+        {"emplace", "Achilles Heal"},
+        {"supline", "High And Dry"},
+        {"keep", "Assassination"},
+        {"tester", "Hero Warship"},
+        {"foot", "Hamburger Hill"},
+        {"final", "Well, Well, Well!"},
+};
+
 typedef struct MapTile {
 
 } MapTile;
@@ -27,3 +64,7 @@ typedef struct MapBlock {
 typedef struct Map {
 
 } Map;
+
+void LoadMap(const char *name) {
+
+}
