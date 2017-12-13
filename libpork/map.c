fd86ffe5e16178f3489115533223d5e3ef08b568
hogsy <markelswo@gmail.com>
2017-12-13 21:47:01 +0000

    Added more map names + descriptions

--- libpork/map.c
+++ libpork/map.c
@@ -16,41 +16,108 @@
  */
 #include "engine.h"
 
+#define MAP_MODE_SINGLEPLAYER   (1 << 1)
+#define MAP_MODE_DEATHMATCH     (1 << 2)
+
+#define MAP_MODE_SURVIVAL_NOVICE    (1 << 3)
+#define MAP_MODE_SURVIVAL_EXPERT    (1 << 4)
+#define MAP_MODE_SURVIVAL_STRATEGY  (1 << 5)
+
+#define MAP_MODE_GENERATED  (1 << 6)
+
 typedef struct MapEntry {
     const char *name;
     const char *description;
+    unsigned int mode_flags;
 } MapEntry;
 
 // for now these are hard-coded, but
 // eventually we'll do this through a
 // script instead
 MapEntry maps_list[]={
-        {"camp", "Boot camp"},
-        {"estu", "The War Foundation"},
-        {"road", "Routine Patrol"},
-        {"trench", "Trench Warfare"},
-        {"devi", "Morning Glory!"},
-        {"rumble", "Island Invasion"},
-        {"zulus", "Under Siege"},
-        {"twin", "Communication Breakdown"},
-        {"sniper", "The Spying Game"},
-        {"mashed", "The Village People"},
-        {"guns", "Bangers 'N' Mash"},
-        {"liberate", "Saving Private Rind"},
-        {"oasis", "Just Deserts"},
-        {"fjords", "Glacier Guns"},
-        {"eyrie", "Battle Stations"},
-        {"bay", "Fortified Swine"},
-        {"medix", "Over The Top"},
-        {"bridge", "Geneva Convention"},
-        {"desval", "I Spy..."},
-        {"snake", "Chemical Compound"},
-        {"emplace", "Achilles Heal"},
-        {"supline", "High And Dry"},
-        {"keep", "Assassination"},
-        {"tester", "Hero Warship"},
-        {"foot", "Hamburger Hill"},
-        {"final", "Well, Well, Well!"},
+        // Single-player
+
+        {"camp", "Boot camp", MAP_MODE_SINGLEPLAYER},
+        {"estu", "The War Foundation", MAP_MODE_SINGLEPLAYER},
+        {"road", "Routine Patrol", MAP_MODE_SINGLEPLAYER},
+        {"trench", "Trench Warfare", MAP_MODE_SINGLEPLAYER},
+        {"devi", "Morning Glory!", MAP_MODE_SINGLEPLAYER},
+        {"rumble", "Island Invasion", MAP_MODE_SINGLEPLAYER},
+        {"zulus", "Under Siege", MAP_MODE_SINGLEPLAYER},
+        {"twin", "Communication Breakdown", MAP_MODE_SINGLEPLAYER},
+        {"sniper", "The Spying Game", MAP_MODE_SINGLEPLAYER},
+        {"mashed", "The Village People", MAP_MODE_SINGLEPLAYER},
+        {"guns", "Bangers 'N' Mash", MAP_MODE_SINGLEPLAYER},
+        {"liberate", "Saving Private Rind", MAP_MODE_SINGLEPLAYER},
+        {"oasis", "Just Deserts", MAP_MODE_SINGLEPLAYER},
+        {"fjords", "Glacier Guns", MAP_MODE_SINGLEPLAYER},
+        {"eyrie", "Battle Stations", MAP_MODE_SINGLEPLAYER},
+        {"bay", "Fortified Swine", MAP_MODE_SINGLEPLAYER},
+        {"medix", "Over The Top", MAP_MODE_SINGLEPLAYER},
+        {"bridge", "Geneva Convention", MAP_MODE_SINGLEPLAYER},
+        {"desval", "I Spy...", MAP_MODE_SINGLEPLAYER},
+        {"snake", "Chemical Compound", MAP_MODE_SINGLEPLAYER},
+        {"emplace", "Achilles Heal", MAP_MODE_SINGLEPLAYER},
+        {"supline", "High And Dry", MAP_MODE_SINGLEPLAYER},
+        {"keep", "Assassination", MAP_MODE_SINGLEPLAYER},
+        {"tester", "Hero Warship", MAP_MODE_SINGLEPLAYER},
+        {"foot", "Hamburger Hill", MAP_MODE_SINGLEPLAYER},
+        {"final", "Well, Well, Well!", MAP_MODE_SINGLEPLAYER},
+
+        // Multi-player
+
+        {"iceflow", "Ice-Flow", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"archi", "You Hillock", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"dbowl", "Death Bowl", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"mlake", "Frost Fight", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"lake", "The Lake", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"ice", "Chill Hill", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"sepia1", "Square Off", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"oneway", "One Way System", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"ridge", "Ridge Back", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+        {"island", "Island Hopper", MAP_MODE_SURVIVAL_EXPERT | MAP_MODE_DEATHMATCH},
+
+        {"play1", "Play Pen", MAP_MODE_SURVIVAL_NOVICE},
+        {"play2", "Duvet Fun", MAP_MODE_SURVIVAL_NOVICE},
+        {"lunar1", "Pigs in Space", MAP_MODE_SURVIVAL_NOVICE},
+        {"hell3", "Skulduggery", MAP_MODE_SURVIVAL_NOVICE},
+        {"hell2", "Pigin' Hell", MAP_MODE_SURVIVAL_NOVICE},
+        {"creepy2", "Graveyard Shift", MAP_MODE_SURVIVAL_NOVICE},
+
+        {"boom", "Friendly Fire", MAP_MODE_SURVIVAL_STRATEGY},
+        {"lecprod", "Bridge The Gap", MAP_MODE_SURVIVAL_STRATEGY},
+        {"bhill", "Dam Busters", MAP_MODE_SURVIVAL_STRATEGY},
+        {"bute", "Moon Buttes", MAP_MODE_SURVIVAL_STRATEGY},
+        {"maze", "Hedge Maze", MAP_MODE_SURVIVAL_STRATEGY},
+        {"cmass", "Cratermass", MAP_MODE_SURVIVAL_STRATEGY},
+        {"artgun", "Doomed", MAP_MODE_SURVIVAL_STRATEGY},
+        {"dval", "Death Valley", MAP_MODE_SURVIVAL_STRATEGY},
+        {"dval2", "Death Valley 2", MAP_MODE_SURVIVAL_STRATEGY},
+
+        {"demo", NULL, 0},
+        {"demo2", NULL, 0},
+        {"easy", NULL, 0},
+
+        {"genbrack", NULL, MAP_MODE_GENERATED},
+        {"genchalk", NULL, MAP_MODE_GENERATED},
+        {"gendesrt", NULL, MAP_MODE_GENERATED},
+        {"genlava", NULL, MAP_MODE_GENERATED},
+        {"genmud", NULL, MAP_MODE_GENERATED},
+        {"gensnow", NULL, MAP_MODE_GENERATED},
+
+        {"hillbase", NULL, 0},
+
+        {"airaid", NULL, 0},
+        {"canal", NULL, 0},
+        {"climb", NULL, 0},
+        {"faceoff", NULL, 0},
+        {"hell1", NULL, 0},
+        {"hiroad", NULL, 0},
+        {"hwd", NULL, 0},
+        {"newsnu", NULL, 0},
+        {"pdrag", NULL, 0},
+        {"quack", NULL, 0},
+        {"tdd", NULL, 0},
 };
 
 typedef struct MapTile {
@@ -66,5 +133,4 @@ typedef struct Map {
 } Map;
 
 void LoadMap(const char *name) {
-
 }
