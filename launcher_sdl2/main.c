95455a958832cb2c4a120cd86c9be892c669cf86
hogsy <markelswo@gmail.com>
2018-03-11 22:59:19 +0000

    further rendering fixups

--- launcher_sdl2/main.c
+++ launcher_sdl2/main.c
@@ -14,9 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <pork/pork.h>
-
 #include <SDL2/SDL.h>
+
+#include "pork/pork.h"
 
 SDL_Window *window = NULL;
 SDL_GLContext *context = NULL;
