ce64fe08d4a637e277a0970357eaa3d649874787
hogsy <markelswo@gmail.com>
2019-02-24 22:18:41 +0000

    changes towards statically linking third-party libs on windows

--- src/engine/client/audio.cpp
+++ src/engine/client/audio.cpp
@@ -26,6 +26,9 @@
 //# include <OpenAL/alext.h>
 //# include <OpenAL/efx-presets.h>
 #else
+#   ifdef WIN32
+#       define AL_LIBTYPE_STATIC
+#   endif
 #   include <AL/al.h>
 #   include <AL/alc.h>
 #   include <AL/alext.h>
