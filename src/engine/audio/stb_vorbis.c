8d64c31328c3866c74562e9f683ee55f7f58a005
hogsy <markelswo@gmail.com>
2019-09-25 23:08:39 +0100

    fixed warning for temp_free in stb_vorbis

--- src/engine/audio/stb_vorbis.c
+++ src/engine/audio/stb_vorbis.c
@@ -889,7 +889,7 @@ static int error(vorb *f, enum STBVorbisError e)
 #define array_size_required(count,size)  (count*(sizeof(void *)+(size)))
 
 #define temp_alloc(f,size)              (f->alloc.alloc_buffer ? setup_temp_malloc(f,size) : alloca(size))
-#define temp_free(f,p)                  0
+#define temp_free(f,p)                  (void)(0)
 #define temp_alloc_save(f)              ((f)->temp_offset)
 #define temp_alloc_restore(f,p)         ((f)->temp_offset = (p))
 
