4dda92613d574e214c337d17344e2e0251522436
DummkopfOfHachtenduden <DummkopfOfHachtenduden@users.noreply.github.com>
2020-02-17 07:26:20 +0100

    SetLooping now called from AudioSource constructor

--- src/engine/audio/audio.cpp
+++ src/engine/audio/audio.cpp
@@ -75,9 +75,8 @@ AudioSource::AudioSource( const AudioSample* sample, PLVector3 pos, PLVector3 ve
 	SetVelocity( vel );
 	SetGain( gain );
 	SetPitch( pitch );
+	SetLooping( looping );
 
-	alSourcei( al_source_id_, AL_LOOPING, looping );
-	OALCheckErrors();
 	alSourcef( al_source_id_, AL_REFERENCE_DISTANCE, 300.0f );
 	OALCheckErrors();
 	alSourcef( al_source_id_, AL_ROLLOFF_FACTOR, 1.0f );
@@ -156,8 +155,8 @@ void AudioSource::SetPitch( float pitch ) {
 	pitch_ = pitch;
 }
 
-void AudioSource::SetLooping(bool looping) {
-	alSourcef(al_source_id_, AL_LOOPING, looping);
+void AudioSource::SetLooping( bool looping ) {
+	alSourcef( al_source_id_, AL_LOOPING, looping );
 	OALCheckErrors();
 }
 
