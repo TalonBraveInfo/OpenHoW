9a89bac31d5e8074e52246e9be500c234f3e5bff
hogsy <markelswo@gmail.com>
2020-10-21 16:38:15 +0100

    Clean up headers, kill engine header.

--- src/engine/Utilities.h
+++ src/engine/Utilities.h
@@ -20,32 +20,32 @@
 #include <PL/platform.h>
 #include <PL/platform_console.h>
 
+#include "MathUtilities.h"
+
 #define u_unused( ... ) (void)( __VA_ARGS__ )
 
-#define u_fclose(FILE)  if((FILE) != NULL) { fclose((FILE)); (FILE) = NULL; }
-#define u_free(DATA)    free((DATA)); (DATA) = NULL
+#define u_fclose( FILE )  if((FILE) != NULL) { fclose((FILE)); (FILE) = NULL; }
+#define u_free( DATA )    free((DATA)); (DATA) = NULL
 
-#ifdef __cplusplus
-static inline std::string u_stringtolower(std::string s) {
-  std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-  return s;
+static inline std::string u_stringtolower( std::string s ) {
+	std::transform( s.begin(), s.end(), s.begin(), ::tolower );
+	return s;
 }
-#endif
 
 PL_EXTERN_C
 
-void* u_realloc(void* ptr, size_t new_size, bool abort_on_fail);
-void* u_alloc(size_t num, size_t size, bool abort_on_fail);
+void *u_realloc( void *ptr, size_t new_size, bool abort_on_fail );
+void *u_alloc( size_t num, size_t size, bool abort_on_fail );
 
-const char* u_scan(const char* path, const char** preference);
-const char* u_find2(const char* path, const char** preference, bool abort_on_fail);
+const char *u_scan( const char *path, const char **preference );
+const char *u_find2( const char *path, const char **preference, bool abort_on_fail );
 
 char *u_new_filename( char *dst, const char *src, const char *ext );
 
 PL_EXTERN_C_END
 
 #ifdef _DEBUG
-#   define u_assert(a, ...) if(!((a))) { Warning(__VA_ARGS__); Print("Assertion hit in \"%s\" on line %d\n", PL_FUNCTION, __LINE__); } assert((a))
+#   define u_assert( a, ... ) if(!((a))) { Warning(__VA_ARGS__); Print("Assertion hit in \"%s\" on line %d\n", PL_FUNCTION, __LINE__); } assert((a))
 #else
 #   define u_assert(a, ...) if(!((a))) { LogWarn(__VA_ARGS__); }
 #endif
